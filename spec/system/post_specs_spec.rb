651a1ec058a6a0fd6566bb84c4e1575a69905120
moe <hamabatamoe@gmail.com>
2025-11-10 22:55:07 +0900

    System Specのテスト

--- spec/system/post_specs_spec.rb
+++ spec/system/post_specs_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "PostSpecs", type: :system do
+RSpec.describe "Posts", type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "投稿の詳細ページにタイトルと本文が表示される" do
+    # テスト用のデータを作る
+    post=Post.create!(title: "RSpec入門", content: "System Specのテスト")
+
+    # その投稿のページにアクセス
+    visit post_path(post)
+
+    # ページに正しい内容があるか確認
+    expect(page).to have_content("RSpec入門")
+    expect(page).to have_content("System Specのテスト")
+  end
 end
