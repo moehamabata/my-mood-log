e18e07ad3e0e7f2ee42156c3cd2ba7ee0a082b60
moe <hamabatamoe@gmail.com>
2025-11-15 17:54:41 +0900

    関連するコントローラのアクションの反映

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,48 @@
 class PostsController < ApplicationController
+  # 一覧
   def index
-  @posts=Post.all
+    @posts=Post.all
   end
 
-def show
-  @post=Post.find(params[:id])
+  # 詳細
+  def show
+    @post=Post.find(params[:id])
   end
+
+  # 新規作成フォーム
+  def new
+    @post=Post.new
+  end
+
+  # 投稿保存
+  def create
+    @post=Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  # 編集フォーム
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  # 投稿更新
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  # ストロングパラメータ
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
+
 end
