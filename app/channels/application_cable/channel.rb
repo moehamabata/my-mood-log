7a311855657b64aa11665912a3e75b79c84b2afe
moe <hamabatamoe@gmail.com>
2026-01-05 18:48:03 +0900

    追記

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
+# ApplicationCableという名前のモジュールを定義する
   class Channel < ActionCable::Channel::Base
+# 親分となる各クラス（ActionCable::Channel）を定義し、その親分がRails本体が用意しているActionCable::Baseを継承する
   end
 end
+
+# moduleが使われる理由としては、Action Cable関連のクラスをひとまとめしたり、共通のメソッドを定義して他のクラスで再利用できるようにするため
+# ActionCable::Channel::Base → Railsのリアル通信機能であるAction Cableにおけるチャネルの親クラスのこと
+# チャネルとは、情報やデータ、信号といった固定的な伝達・流通経路のことを指す
+
