2143d9bc8e560b13ed9999f39b81627919386dbc
moe <hamabatamoe@gmail.com>
2026-01-15 23:40:04 +0900

    コメントアウト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
 # ApplicationControllerにActionControllerの親クラスを継承させる 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # 
 end
