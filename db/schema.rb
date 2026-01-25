f1edb29b3d66af60bf85a68291abd1f6e5ccda63
moe <hamabatamoe@gmail.com>
2026-01-25 22:36:08 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_06_184251) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_25_132647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_06_184251) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "mood"
   end
 end
