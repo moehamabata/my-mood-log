baa8d11e7d4fd2c1ed8b3c7bede4b86449019ed6
moe <hamabatamoe@gmail.com>
2026-01-30 00:23:26 +0900

    エラー修正

--- test/system/user_logins_test.rb
+++ test/system/user_logins_test.rb
@@ -1,11 +1,8 @@
 require "application_system_test_case"
 
-class UserLoginsTest < ApplicationSystemTestCase
+class PostTest < application_system_test_case
   test "トップページに挨拶が表示されること" do
-    # 1. トップページ（http://localhost:3000）にアクセスする
     visit root_url
-
-    # 2. 画面内に「こんにちは!」という文字があるかチェックする
-    assert_text "こんにちは!Railsの世界へようこそ"
+    assert_text "こんにちは！Railsの世界へようこそ"
   end
 end
