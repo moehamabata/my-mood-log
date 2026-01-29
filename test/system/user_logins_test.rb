2ec42733c6efdc731d22deccc26579829a2c9eda
moe <hamabatamoe@gmail.com>
2026-01-30 00:28:39 +0900

    エラー修正

--- test/system/user_logins_test.rb
+++ test/system/user_logins_test.rb
@@ -1,6 +1,6 @@
 require "application_system_test_case"
 
-class PostTest < application_system_test_case
+class PostTest < ApplicationSystemTestCase
   test "トップページに挨拶が表示されること" do
     visit root_url
     assert_text "こんにちは！Railsの世界へようこそ"
