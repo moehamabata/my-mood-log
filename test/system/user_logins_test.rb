597800ae18a5c75336e63b9651535a7f4846bfd3
moe <hamabatamoe@gmail.com>
2026-01-30 23:57:48 +0900

    エラー修正

--- test/system/user_logins_test.rb
+++ test/system/user_logins_test.rb
@@ -1,8 +1,11 @@
 require "application_system_test_case"
 
-class PostsTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit root_url
+class UserLoginsTest < ApplicationSystemTestCase
+  test "トップページが表示されること" do
+    # root_url は、あなたが bin/rails s で見ている最初のページのこと
+    visit root_url 
+
+    # 画面にこの文字があるか、ロボットに探させる
     assert_text "こんにちは!Railsの世界へようこそ"
   end
 end
