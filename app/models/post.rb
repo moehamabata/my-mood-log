b72f856a427f293aed80924799ad659a18edfa8c
moe <hamabatamoe@gmail.com>
2026-02-02 23:52:00 +0900

    コメントアウト修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 # ApplicationRecordをPostクラスに継承させる
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
-  # タイトルの空文字は保存不可、文字制限は3文字以上、50文字以下
+  # タイトルの空文字は保存不可で入れることが必須。文字制限は3文字以上、50文字以下
   validates :content, presence: true, length: { maximum: 450 }
   # 本文の空文字は保存不可、文字制限は450文字以下
 end
