d7cbfd7d16d1f9af0ab2284b98b0475f6fa5c4ee
moe <hamabatamoe@gmail.com>
2026-02-08 18:57:19 +0900

    moodのバリデーション追加]

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
 # ApplicationRecordをPostクラスに継承させる
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
+  belongs_to :user
+  # 日記は一人のユーザーに属している
+  validates :title, presence: true, length: { minimum: 1, maximum: 50 }, uniqueness: true
   # タイトルの空文字は保存不可で入れることが必須。文字制限は3文字以上、50文字以下
   validates :content, presence: true, length: { maximum: 500 }
   # 本文の空文字は保存不可、文字制限は500文字以下
+  validates :mood, presense : true,
+  # moodのバリデーション追加"
 end
