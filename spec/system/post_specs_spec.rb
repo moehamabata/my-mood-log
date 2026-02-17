abe4c0d9450e2d6682c230fb6dc05cda6b930c90
moe <hamabatamoe@gmail.com>
2026-02-18 04:46:06 +0900

    エラーの修正

--- spec/system/post_specs_spec.rb
+++ spec/system/post_specs_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :system do
+let(:user) do
+User.create(
+email: 'test@example.com',
+password: 'password',
+password_confirmation: 'password'
+)
+end
 
-# テスト用のユーザーを準備（ログインが必要な場合）
-let(:user){User.create(email:'test@example.com',password:'password'
-password_confirmation:'password')}
-
-it "ブラウザが立ち上がり、投稿の詳細が表示されること"do
-# テスト用のデータを作る
-post_item=Post.create!(title:"RSpec入門",content:"System Specのテスト",
-mood:"楽しい",user:user)
+it "投稿の詳細が表示されること" do
+post_item = Post.create!(
+title: "RSpec入門",
+content: "System Specのテスト",
+mood: "楽しい",
+user: user
+)
 
 end
 end
