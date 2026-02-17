b83bba1631966f75e2aa78b51320604aed54fd5c
moe <hamabatamoe@gmail.com>
2026-02-18 03:59:57 +0900

    CRUDテストを追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -7,6 +7,12 @@ let!(:post_item){Post.create(
 title:'初期タイトル',content:'初期内容',mood:'普通',user:user)}
 
 describe "Post CRUDテスト"do
+describe "GET/posts"do
+it "正常にレスポンスを返すこと"do
+get posts_path
+expect(response).to have_http_status(:success)
+end
+end
 
 end
 end
