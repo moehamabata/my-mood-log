a8a6e4457887bfcbab2881664f0c491690725092
moe <hamabatamoe@gmail.com>
2026-02-18 02:24:42 +0900

    ミスの修正

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,8 +1,8 @@
-require`rails_helper` # Railsのテストに必要な設定ファイルを読み込む
+require 'rails_helper' # Railsのテストに必要な設定ファイルを読み込む
 
 RSpec.describe Post, type: :model do # Postモデルに関するテストを書いていく
-describe`バリデーションのテスト`do # テストを分かりやすく整理するための見出し。「バリデーションについてチェックしていく」
-let(:user) {FactoryBot.create(:user)} # テスト用のユーザーを一人、裏側で準備している
+  describe 'バリデーションのテスト' do # テストを分かりやすく整理するための見出し。「バリデーションについてチェックしていく」
+    let(:user) { FactoryBot.create(:user) } # テスト用のユーザーを一人、裏側で準備している
 
 end
 end
