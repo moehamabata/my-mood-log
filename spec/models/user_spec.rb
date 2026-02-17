f98c823c3d139d8b9bf7b47f36529febc665c51b
moe <hamabatamoe@gmail.com>
2026-02-18 02:36:50 +0900

    単体テストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe'バリデーションのテスト'do
+context'保存に成功する場合'do
+it 'メールアドレスとパスワードがあれば保存できること'do
+user=FactoryBot.build(:user)
+expect(user).to be_valid
+end
+end
+
+end
 end
