141ee295b518acbfd62a1e2ab06e7a25b6768c1d
moe <hamabatamoe@gmail.com>
2026-02-18 02:43:03 +0900

    FactoryBotを辞めて手動でデータを入力する形 に変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
 describe'バリデーションのテスト'do
 context'保存に成功する場合'do
 it 'メールアドレスとパスワードがあれば保存できること'do
-user=FactoryBot.build(:user)
+user=User.new(email:'test@example.com',password'password:',password_confirmation:'password')
 expect(user).to be_valid
 end
 end
