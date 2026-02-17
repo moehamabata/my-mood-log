0fdc4789f278fddceef40863f401a75d2fb619bb
moe <hamabatamoe@gmail.com>
2026-02-18 00:32:44 +0900

    タイトルが他のユーザーと被っても投稿できるようにした

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 class Post < ApplicationRecord
   belongs_to :user # 日記は一人のユーザーに属している
   # タイトルの空文字は保存不可で入れることが必須。文字制限は3文字以上、50文字以下
-  validates :title, presence: true, length: { minimum: 1, maximum: 50 }, uniqueness: true
+  validates :title, presence: true, length: { minimum: 1, maximum: 50 }
   # 本文の空文字は保存不可、文字制限は500文字以下
   validates :content, presence: true, length: { maximum: 500 }
   # moodのバリデーション追加"
