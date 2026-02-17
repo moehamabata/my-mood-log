7d8b9b02820a2cb6fd104b076d3b93ba326d12d9
moe <hamabatamoe@gmail.com>
2026-02-18 03:55:28 +0900

    新規作成・詳細・更新・削除を追加し、contextを使って成功と失敗を分ける

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+let(:user){User.create(email:'test@example.com',password:'password',
+password_confirmation:'password')}
+let!(:post_item){Post.create(
+title:'初期タイトル',content:'初期内容',mood:'普通',user:user)}
 
+describe "Post CRUDテスト"do
+
+end
 end
