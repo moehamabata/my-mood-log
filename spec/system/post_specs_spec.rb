8ef24310a30ee24875514052c46f1b2a6796d651
moe <hamabatamoe@gmail.com>
2026-02-18 06:27:06 +0900

    ページを見にいく命令を追加

--- spec/system/post_specs_spec.rb
+++ spec/system/post_specs_spec.rb
@@ -9,13 +9,24 @@ password_confirmation: 'password'
 )
 end
 
-it "投稿の詳細が表示されること" do
+it "投稿の詳細が表示されること" dosuru
+
 post_item = Post.create!(
 title: "RSpec入門",
 content: "System Specのテスト",
 mood: "楽しい",
 user: user
 )
+
+# 投稿の詳細ページに実際にアクセスする命令
+visit post_path(post_item)
+
+# 画面に期待する文字があるか確認
+expect(page).to have_content("RSpec入門")
+expect(page).to habe_content("System Specのテスト")
+
+# 動きを目で確認するために5秒間ブラウザを止める
+sleep 5
 
 end
 end
