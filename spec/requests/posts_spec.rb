dbacc368b77aed2daa1e269917f664d1e9370a15
moe <hamabatamoe@gmail.com>
2026-02-25 00:01:44 +0900

    コード修正

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,23 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-let(:user){User.create(email:'test@example.com',password:'password',
-password_confirmation:'password')}
-let!(:post_item){Post.create(
-title:'初期タイトル',content:'初期内容',mood:'普通',user:user)}
+  let(:user){ User.create(
+    email:'test@example.com',
+    password:'password',
+    password_confirmation:'password'
+  )}
+  let!(:post_item){ Post.create(
+    title:'初期タイトル',
+    content:'初期内容',
+    mood:'普通',
+    user:user
+  )}
 
 before do
-post user_session_path,params:
-{user:{email:user.email,password:user.password}}
-end
+  post user_session_path, params:
+    {user:
+      {
+        email:user.email,
+        password:user.password
+      }
+    }
+  end
 
-describe "Post CRUDテスト"do
-describe "GET/posts"do
-it "正常にレスポンスを返すこと"do
-get posts_path
-expect(response).to have_http_status(:success)
-end
-end
+  describe "Post CRUDテスト"do
+    describe "GET/posts"do
+    it "正常にレスポンスを返すこと"do
+      get posts_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-end
+  end
 end
