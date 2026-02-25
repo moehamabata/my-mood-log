7647e4ad59b8d4489dcd0f978df7cf2c44be2328
moe <hamabatamoe@gmail.com>
2026-02-25 22:13:02 +0900

    改行

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,19 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-require 'capybara/rspec'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+  require 'spec_helper'
+  require 'capybara/rspec'
+  ENV['RAILS_ENV'] ||= 'test'
+  require_relative '../config/environment'
 
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+  # Prevent database truncation if the environment is production
+  abort("The Rails environment is running in production mode!") if Rails.env.production?
 
-require 'rspec/rails'
+  require 'rspec/rails'
 
-# Ensures that the test database schema matches the current schema file.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
+  # Ensures that the test database schema matches the current schema file.
+  begin
+    ActiveRecord::Migration.maintain_test_schema!
+  rescue ActiveRecord::PendingMigrationError => e
+    abort e.to_s.strip
 end
 
 RSpec.configure do |config|
