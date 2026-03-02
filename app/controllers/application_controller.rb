3689db016ed277f985db9c1831c5456809fef9f0
moe <hamabatamoe@gmail.com>
2026-03-02 22:44:19 +0900

    ログイン後にブラウザバックをした際に、ログイン画面に戻らないよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
 # ApplicationControllerにActionControllerの親クラスを継承させる 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  # 
+  before_action :set_no_cache
+
+  private
+
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+  end
 end
