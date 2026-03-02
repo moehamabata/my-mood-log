d497ce30474bc4ea364c89f75df248e240fcbf55
moe <hamabatamoe@gmail.com>
2026-03-02 23:17:22 +0900

    wip i18nの追加

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Myapp
     config.load_defaults 7.2
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
