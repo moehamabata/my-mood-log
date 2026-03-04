97ad2389bf3141e07023201e4bbf52349fe425b2
moe <hamabatamoe@gmail.com>
2026-03-04 21:25:32 +0900

    ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 # ApplicationControllerにActionControllerの親クラスを継承させる 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
   before_action :set_no_cache
 
   private
