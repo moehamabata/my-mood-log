b41632b6e305df7797275481d1308e0c5ebdbd73
moe <hamabatamoe@gmail.com>
2026-03-24 19:33:31 +0900

    Fix migration command with RAILS_ENV

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rails db:migrate
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# データベースを本番環境でマイグレーションする指示
+RAILS_ENV=production bundle exec rails db:migrate
