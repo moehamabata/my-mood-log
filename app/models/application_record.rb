1a3851e76df1850b280cd883da873f9b2fc2d28d
moe <hamabatamoe@gmail.com>
2026-04-06 17:36:49 +0900

    application_record.rbのコンフリクトを解消

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-<<<<<<< HEAD
-# ActiveRecordの基礎をApplicationRecordクラスに継承させる
-=======
->>>>>>> 15e83568e23540d107d48e9b5f9898576315dcad
   primary_abstract_class
 end
