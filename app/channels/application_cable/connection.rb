9fe2fad2e09281164d06b007d0b848918d165820
moe <hamabatamoe@gmail.com>
2026-04-06 17:55:16 +0900

    connection.rbのコンフリクトを解消

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,14 +1,4 @@
 module ApplicationCable
-<<<<<<< HEAD
-# ApplicationCableという名前のモジュールを定義する
   class Connection < ActionCable::Connection::Base
-# Connectionクラスにチャネルの親クラスを継承する
   end
 end
-=======
-# ActionCableをモジュールに分け、Ruby on Railsに含まれているリアルタイム通信を可能にする
-  class Connection < ActionCable::Connection::Base
-# ActionCable::Connection::BaseのRailsの基礎をClass Connectionが継承する 
-  end
-end
->>>>>>> 15e83568e23540d107d48e9b5f9898576315dcad
