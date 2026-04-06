3d8c93364c4107c045671ab54c4338fd35ed28b7
moe <hamabatamoe@gmail.com>
2026-04-06 20:04:38 +0900

    ログイン必須の設定を適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
-class ApplicationController < ActionController::Base
-# ApplicationControllerにActionControllerの親クラスを継承させる 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+class ApplicationController < ActionController::Base # ApplicationControllerにActionControllerの親クラスを継承させる 
+
+  before_action :authenticate_user!
   before_action :set_no_cache
 
   private
