75ab77f5b2ebb5f074b519b4897fdec147d4b70f
moe <hamabatamoe@gmail.com>
2026-04-06 18:07:27 +0900

    FIX: 全てのコンフリクトを解消し、文法エラーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 module ApplicationHelper
-<<<<<<< HEAD
 # ApplicationHelperというモジュールを定義する
-=======
->>>>>>> 15e83568e23540d107d48e9b5f9898576315dcad
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-<<<<<<< HEAD
 # ActionMailerの基礎をApplicationMailerに継承させる
   default from: "from@example.com"
-  layout "mailer"
-=======
-# ACtionMailer::BaseのRails on Railsをclass ApplicationMailerに継承させる
-  default from: "from@example.com"
-# 特に変更がなければ "from@example.com"が使われる
-  layout "mailer"
-# 
->>>>>>> 15e83568e23540d107d48e9b5f9898576315dcad
+  layout "maile"
 end
