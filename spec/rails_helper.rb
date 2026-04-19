9951b4ce3ea1f0136519815e505d54a53b267237
moe <hamabatamoe@gmail.com>
2026-04-19 21:30:12 +0900

    システムテストをheadlessモードで実行するよう修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,21 @@ RSpec.configure do |config|
 
   # System spec (Capybara + Selenium)
   config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+    driven_by :headless_chrome
   end
 end
+
+# Capybaraのドライバ登録はそのままでOK
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :headless_chrome
